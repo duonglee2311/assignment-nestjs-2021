80abb33fac83577cd7e7935723ab4628b852accd
CQ <38405415+nightwalker89@users.noreply.github.com>
2021-11-08 23:44:07 +0700

    implement swagger-ui

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
@@ -5,6 +7,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs Assigment API')
+    .setDescription('Nestjs Assigment API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
