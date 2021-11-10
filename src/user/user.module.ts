cc780d20fae5f5cae608492306827e6e55bddfbc
nightwalker89 <internal+kh@coredevelop.com>
2021-11-11 06:45:02 +0700

    user service constructor

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,12 @@
+import { UserEntity } from './entities/user.entity';
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  exports: [TypeOrmModule],
   controllers: [UserController],
   providers: [UserService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,16 @@
+import { UserEntity } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Injectable } from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private _repository: Repository<UserEntity>,
+  ) {}
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
