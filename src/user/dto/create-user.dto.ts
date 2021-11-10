da4fbc1ccb5d1ce75cf06360f2ff73060102abb1
nightwalker89 <internal+kh@coredevelop.com>
2021-11-10 07:36:27 +0700

    create user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,35 @@
-export class CreateUserDto {}
+import {
+  validate,
+  validateOrReject,
+  Contains,
+  IsInt,
+  Length,
+  IsEmail,
+  IsFQDN,
+  IsDate,
+  Min,
+  Max,
+  MinLength,
+  IsString,
+  Matches,
+} from 'class-validator';
+export class CreateUserDto {
+  @IsString()
+  @Length(3, 100)
+  name: string;
+
+  @IsString()
+  @Length(3, 20)
+  @Matches(/^[a-zA-Z]([_]?[a-zA-Z0-9]+)*$/)
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @MinLength(8)
+  @IsString()
+  password: string;
+
+  @IsDate()
+  birthday: string;
+}
