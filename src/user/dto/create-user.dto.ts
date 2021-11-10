a2118a04d02c1ed5ce4ab2ad77b19ecd062d41eb
nightwalker89 <internal+kh@coredevelop.com>
2021-11-11 06:34:46 +0700

    update error message

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -13,7 +13,10 @@ export class CreateUserDto {
 
   @IsString()
   @Length(3, 20)
-  @Matches(/^[a-zA-Z]([_]?[a-zA-Z0-9]+)*$/)
+  @Matches(/^[a-zA-Z]([_]?[a-zA-Z0-9]+)*$/, {
+    message:
+      'The first character of the username must be a alphabet char. Username must contains at least 4 characters',
+  })
   username: string;
 
   @IsEmail()
