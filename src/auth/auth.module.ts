d180831d0a23c260114fc39d2d54c4c350c82bdf
CQ <38405415+nightwalker89@users.noreply.github.com>
2021-11-15 23:05:43 +0700

    implement login and jwt validate

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
@@ -22,8 +23,9 @@ import { UserService } from 'src/user/user.service';
         signOptions: { expiresIn: '60m' },
       }),
     }),
+    ConfigModule,
   ],
-  providers: [AuthService, UserService, LocalStrategy],
+  providers: [AuthService, UserService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT'),
+    });
+  }
+
+  async validate(payload: any) {
+    return { id: payload.id, username: payload.username, name: payload.name };
+  }
+}
