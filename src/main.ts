020afcc0ca4e3179c74d45f461c5ce8072f88cb0
nightwalker89 <cuong@zodinet.com>
2021-11-19 14:05:52 +0700

    fix spell

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('Nestjs Assigment API')
-    .setDescription('Nestjs Assigment API description')
+    .setDescription('Nestjs Assignment API description')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
